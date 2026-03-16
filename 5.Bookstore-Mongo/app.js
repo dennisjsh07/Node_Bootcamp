7df4d173de781f7266f2c3264cbf0e419d70cfa7
dennis <dennisjshofficial@gmail.com>
2026-03-16 08:34:25 +0530

    added config and helper functions to upload image using cloudinary

--- 5.Bookstore-Mongo/app.js
+++ 5.Bookstore-Mongo/app.js
@@ -12,13 +12,11 @@ app.use(express.json());
 
 app.use("/api/v1", rootRouter);
 
-/*
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const errMsg = err.message || "Internal server error";
   res.status(statusCode).json({ msg: errMsg });
 });
-*/
 
 app.listen(port, () => {
   console.log(`app running on ${port}`);
---
+++ 5.Bookstore-Mongo/helpers/cloudinary.js
@@ -0,0 +1,17 @@
+const cloudinary = require("../config/cloudinary");
+
+// helper method to upload image to cloudinary
+const uploadToCloudinary = async (filePath) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath);
+    return {
+      url: result.secure_url,
+      publicId: result.public_id,
+    };
+  } catch (err) {
+    console.error("Error while uploading to cloudinary", err);
+    throw new Error("Error while uploading to cloudinary");
+  }
+};
+
+module.exports = { uploadToCloudinary };
